273dd2fd153aa24ef9311a8967ceb87e5fea6739
Nahuel <vallesnahuel1998@gmail.com>
2021-02-12 11:56:18 -0300

    Change the URL in the driver

--- LearningSelenium/UnitTest1.cs
+++ LearningSelenium/UnitTest1.cs
@@ -24,6 +24,7 @@ namespace LearningSelenium
         [Test]
         public void Test1()
         {
+            driver.Navigate().GoToUrl("https://demo.aspnetawesome.com/");
             Assert.Pass();
         }
     }
