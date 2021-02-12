92fc8bd9bc1a7d7be4280e9c3ede13359bda7620
Nahuel <vallesnahuel1998@gmail.com>
2021-02-12 11:40:10 -0300

    first commit

---
+++ LearningSelenium/UnitTest1.cs
@@ -0,0 +1,30 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Threading;
+
+namespace LearningSelenium
+{
+    public class Tests
+    {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
